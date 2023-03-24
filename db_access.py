3835c6af91bffac8ad51ac9186852362750b58c3
Aleksander <af@sikom.no>
2023-03-24 17:57:33 +0100

    Add a lot of stuff

--- db_access.py
+++ db_access.py
@@ -1,21 +1,134 @@
 import sqlite3
-from datetime import date, datetime
-con = sqlite3.connect("railway.db")
+from datetime import date, time, datetime
+from typing import Optional
+from decimal import Decimal
 
 def create_customer(id:str, name: str, email: str, phone_no: str):
-    pass
+    con = sqlite3.connect("main.db")
+    cur = con.cursor()
+    query = "INSERT INTO CUSTOMER (Name, Email, PhoneNo) VALUES (?, ?, ?)"
+    cur.execute(query, (name, email, phone_no))
+    con.commit()
+    con.close()
+    print(f"Inserted customer {name} with email {email} and phone number {phone_no} into the database.")
 
-def create_order(order_id: int, customer_id: int, date_time: datetime, trip_year: int, trip_week_kr: int, start_station_name: str, end_station_name: str, route_id: int, weekday: str):
-    pass
+def create_order(order_id: int, customer_id: int, date_time: datetime, trip_year: int, trip_week_nr: int, start_station_name: str, end_station_name: str, route_id: int, weekday: str):
+    con = sqlite3.connect("main.db")
+    cur = con.cursor()
+    query = "INSERT INTO CUSTOMER_ORDER (OrderID, CustomerID, DateTime, TripYear, TripWeekNr, StartStationName, EndStationName, RouteID, Weekday) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    cur.execute(query, (order_id, customer_id, date_time, trip_year, trip_week_nr, start_station_name, end_station_name, route_id, weekday))
+    con.commit()
+    con.close()
+    print(f"Inserted order with ID {order_id} for customer with ID {customer_id} for trip from {start_station_name} to {end_station_name} on {weekday}, year {trip_year}, week {trip_week_nr} into the database.")        
 
-def get_orders_by_customer(customer_id: int):
-    pass
+class Order:
+    def __init__(self, order_id: int, customer_id: int, date_time: datetime, trip_year: int, trip_week_nr: int, start_station_name: str, end_station_name: str, route_id: int, weekday: str):
+        self.order_id = order_id
+        self.customer_id = customer_id
+        self.datetime = date_time
+        self.trip_year = trip_year
+        self.trip_week_nr = trip_week_nr
+        self.start_station_name = start_station_name
+        self.end_station_name = end_station_name
+        self.route_id = route_id
+        self.weekday = weekday
+
+
+def get_all_orders_by_customer(customer_id: int) -> list[Order]:
+    con = sqlite3.connect("main.db")
+    cur = con.cursor()
+    query = "SELECT * FROM CUSTOMER_ORDER WHERE CustomerID = ?"
+    cur.execute(query, (customer_id,))    
+    results = cur.fetchall()
+    con.close()
+    print(f"Fetched {len(results)} orders for customer {customer_id}")
+    # Convert the results into a list of Order objects
+    orders = []
+    for row in results:
+        order = Order(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8])
+        orders.append(order)
+
+    # Return the list of Order objects
+    return orders
 
 class RouteStationTime:
-    route_id: int
-    station_name: str
-    time_of_arrival: str
-    time_of_arrival: str
+    def __init__(self, route_id: int, station_name: str, time_of_arrival: Optional[time], time_of_departure: Optional[time]):
+        self.route_id = route_id
+        self.station_name = station_name
+        self.time_of_arrival = time_of_arrival
+        self.time_of_departure = time_of_departure
+        self.is_start = ((time_of_arrival == None) and (time_of_departure != None))
+        self.is_end = ((time_of_departure == None) and (time_of_arrival != None))
+        
+def get_route_station_times() -> list[RouteStationTime]:
+    con = sqlite3.connect("main.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM ROUTE_STATION_TIME")    
+    route_station_times = []
+    results = cur.fetchall()
+    print(f"Fetched {len(results)} route station times")
+    for row in results:
+        time_of_arrival == None
+        time_of_departure == None
+        if row[2] != None:
+            time_of_arrival = datetime.strptime(row[2], '%H:%M:%S').time()
+        if row[3] != None:
+            time_of_departure = datetime.strptime(row[3], '%H:%M:%S').time()
+        route_station_time = RouteStationTime(row[0], row[1], row[2], row[3])
+        route_station_times.append(route_station_time)
+    con.close()
+    return route_station_times
 
-def get_route_station_times():
-    pass
+class Route:
+    def __init__(self, route_id: int, name: str, operator_id: int, start_station_name: str, end_station_name: str):
+        self.route_id = route_id
+        self.name = name
+        self.operator_id = operator_id
+        self.start_station_name = start_station_name
+        self.end_station_name = end_station_name
+        
+def get_routes() -> list[Route]:
+    con = sqlite3.connect("main.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM ROUTE")    
+    routes = []
+    results = cur.fetchall()
+    print(f"Fetched {len(results)} routes")
+    for row in results:
+        routes.append(RouteStationTime(row[0], row[1], row[2], row[3], row[4]))
+    con.close()
+    return routes
+
+class RouteWeekday:
+    def __init__(self, route_id: int, weekday: str):
+        self.route_id = route_id
+        self.weekday = weekday
+
+def get_route_weekdays() -> list[RouteWeekday]:
+    con = sqlite3.connect("main.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM ROUTE_WEEKDAY")    
+    route_weekdays = []
+    results = cur.fetchall()
+    print(f"Fetched {len(results)} route weekdays")
+    for row in results:
+        route_weekdays.append(RouteWeekday(row[0], row[1]))
+    con.close()
+    return route_weekdays
+
+class Station:
+    def __init__(self, name: str, altitude: Decimal):
+        self.name = name
+        self.altitude = altitude
+
+def get_stations() -> list[Station]:
+    con = sqlite3.connect("main.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM Station")    
+    stations = []
+    results = cur.fetchall()
+    print(f"Fetched {len(results)} stations")
+    for row in results:
+        stations.append(RouteWeekday(row[0], Decimal(row[1]/10)))
+    con.close()
+    return stations
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sqlite3
 import os
-from commands import get_all_station_routes, get_routes_between_stations, register_customer, get_available_seats, register_order, get_customer_orders
+from commands import get_all_station_routes, get_routes_between_stations, register_customer, get_available_seats, register_order, get_future_customer_orders
 
 print("\t**********************************************")
 print("\t***  Railway service DB application  ***")
@@ -15,7 +15,7 @@ def print_choices():
     print("5. register_order customer_number start_station end_station route_id day month year place1 place2....")
     print("For 5. a place consists of the car number and the place number joined by a hyphen, i.e carnr-placenr")
     print("Minimum 1 place")    
-    print("6. get_customer_orders customer_number")
+    print("6. get_future_customer_orders customer_number")
     print("7. quit")
 
 print_choices()
@@ -34,4 +34,4 @@ while choice != "quite":
     if choice.startswith("register_order"):        
         print(get_routes_between_stations(args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[7:]))
     if choice.startswith("get_customer_orders"):        
-        print(get_customer_orders(args[1]))
+        print(get_future_customer_orders(args[1]))
