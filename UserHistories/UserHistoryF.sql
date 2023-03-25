470781470c6d9d48178beb4d7bb153284fb8d8d7
Anders Ziener Kristensen <anders.kristensen@ntnu.no>
2023-03-25 13:35:39 +0100

    Added places in the database

---
+++ UserHistories/UserHistoryF.sql
@@ -0,0 +1,76 @@
+INSERT INTO PLACE (CarTypeName, PlaceNo)
+VALUES
+    --Route 1 | Car 1 | Seating Car
+    ('SJ-chair car-1', 1),
+    ('SJ-chair car-1', 2),
+    ('SJ-chair car-1', 3),
+    ('SJ-chair car-1', 4),
+
+    ('SJ-chair car-1', 5),
+    ('SJ-chair car-1', 6),
+    ('SJ-chair car-1', 7),
+    ('SJ-chair car-1', 8),
+
+    ('SJ-chair car-1', 9),
+    ('SJ-chair car-1', 10),
+    ('SJ-chair car-1', 11),
+    ('SJ-chair car-1', 12),
+
+    --Route 1 | Car 1 | Seating Car
+    ('SJ-chair car-2', 1),
+    ('SJ-chair car-2', 2),
+    ('SJ-chair car-2', 3),
+    ('SJ-chair car-2', 4),
+
+    ('SJ-chair car-2', 5),
+    ('SJ-chair car-2', 6),
+    ('SJ-chair car-2', 7),
+    ('SJ-chair car-2', 8),
+
+    ('SJ-chair car-2', 9),
+    ('SJ-chair car-2', 10),
+    ('SJ-chair car-2', 11),
+    ('SJ-chair car-2', 12),
+
+    --Route 2 | Car 1 | Seating Car
+    ('SJ-chair car-3', 1),
+    ('SJ-chair car-3', 2),
+    ('SJ-chair car-3', 3),
+    ('SJ-chair car-3', 4),
+
+    ('SJ-chair car-3', 5),
+    ('SJ-chair car-3', 6),
+    ('SJ-chair car-3', 7),
+    ('SJ-chair car-3', 8),
+
+    ('SJ-chair car-3', 9),
+    ('SJ-chair car-3', 10),
+    ('SJ-chair car-3', 11),
+    ('SJ-chair car-3', 12),
+
+    --Route 2 | Car 2 | Sleeping Car
+    ('SJ-sleeping car-1', 1),
+    ('SJ-sleeping car-1', 2),
+    ('SJ-sleeping car-1', 3),
+    ('SJ-sleeping car-1', 4),
+
+    ('SJ-sleeping car-1', 5),
+    ('SJ-sleeping car-1', 6),
+    ('SJ-sleeping car-1', 7),
+    ('SJ-sleeping car-1', 8),
+
+    --Route 3 | Car 1 | Seating Car
+    ('SJ-chair car-4', 1),
+    ('SJ-chair car-4', 2),
+    ('SJ-chair car-4', 3),
+    ('SJ-chair car-4', 4),
+
+    ('SJ-chair car-4', 5),
+    ('SJ-chair car-4', 6),
+    ('SJ-chair car-4', 7),
+    ('SJ-chair car-4', 8),
+
+    ('SJ-chair car-4', 9),
+    ('SJ-chair car-4', 10),
+    ('SJ-chair car-4', 11),
+    ('SJ-chair car-4', 12);
